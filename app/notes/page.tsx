572be4cf4e47874d8580046b2c3d9eb763bc7d6e
calltella <yuichi.asa@gmail.com>
2026-02-12 03:54:41 +0000

    noes app

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -7,7 +7,7 @@ import { utcFormatDateTimeWithDay } from "@/lib/utils/date"
 
 export default async function NotesPage() {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/notes`,
+    '/api/notes',
     { cache: "no-store" }
   );
 
